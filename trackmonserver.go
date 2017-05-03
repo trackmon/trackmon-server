9b351044f04d9a4e0dabb3bbe40b94342cf06562
Paul Kramme <pjkramme@gmail.com>
2017-05-03 22:18:26 +0200

    Add mux and costum server
    Add costum server from net/http and gorilla/mux, which should be the
    RESTs base.

--- trackmonserver.go
+++ trackmonserver.go
@@ -1,7 +1,24 @@
 package main
 
-import "log"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	return
+}
 
 func main() {
-  log.Println("Server start")
+	log.Println("TRACKMON SERVER UPSTART")
+	r := mux.NewRouter()
+	r.HandleFunc("/", RootHandler)
+
+	// Make new server
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:80",
+	}
+	srv.ListenAndServe()
 }
