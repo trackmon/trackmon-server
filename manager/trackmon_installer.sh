9cf8d06ca0c8029a7279a66c5856f955fdb82ada
Paul Kramme <pjkramme@gmail.com>
2017-05-06 16:14:18 +0200

    Add database creation code

--- manager/trackmon_installer.sh
+++ manager/trackmon_installer.sh
@@ -1,30 +1,17 @@
 #/bin/bash
 
-echo "TRACKMON INSTALLER"
-echo "This script installs TRACKMON SERVER for you."
+echo "TRACKMON DATABASE SETUP"
+echo "This script setups the TRACKMON SERVER DATABASE for you."
 sleep 2s
 
-if (whiptail --title "Trackmon Installer" --yesno "Do you want to install Trackmon Server? By installing you agree to the license. It can be found at https://github.com/trackmon/trackmon-server" 8 78) then
-  echo "Continuing installation"
+if (whiptail --title "Trackmon Database Setup" --yesno "Do you want to setup the Trackmon Server Database? This requires postgreSQL to be installed!" 8 78) then
+  echo "Continuing setup"
 else
-  echo "User selected No, aborting installation."
+  echo "User selected No, aborting setup."
   exit 1
 fi
-echo -n "Getting TRACKMON MANAGER program... "
-wget -q https://raw.githubusercontent.com/trackmon/trackmon-server/master/manager/trackmon_manager.py
-echo "done."
-if (whiptail --title "Trackmon Installer" --yes-button "Fresh Install" --no-button "Update" --yesno "Is this a fresh install or an update" 8 78) then
-  echo "Making a fresh install"
-  whiptail --title "Trackmon Installer" --menu "What is the Linux distro of your choice?" 25 78 16 "install" "Full Install" "installapi" "Application Server Only" "installdb" "Database Only" "installfrontend" "Website Only" 2>installchoice
-  INSTALLTYPE=$(cat installchoice)
-  rm installchoice
-  exitstatus=$?
-  if [ $exitstatus = 0 ]; then
-    echo "Launching MANAGER with $INSTALLTYPE flag"
-    python3 trackmon_manager.py -$INSTALLTYPE
-  else
-    echo "Aborting the installation."
-  fi
-else
-  echo "Making an update"
-fi
+
+echo "Trying to create postgres user trackmon"
+sudo -u postgres psql -d template1 -c "CREATE USER trackmon CREATEDB;"
+echo "Trying to create database trackmon_server_production"
+sudo -u postgres psql -d template1 -c "CREATE DATABASE trackmon_server_production OWNER trackmon;"
